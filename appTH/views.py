4437b8d1345e86c6744f24f19a932aef53b5a033
JungSubeom <teller75@naver.com>
2021-07-05 15:13:55 +0900

    views.py comment

--- appTH/views.py
+++ appTH/views.py
@@ -8,7 +8,7 @@ import Adafruit_DHT
 import pymysql
 import csv
 from django.utils.encoding import smart_str
-from .models import *
+from .models import * #models.py에 있는 TH_data, TH_state 모델? import
 sys.path.append(os.path.dirname(os.path.abspath(os.path.dirname(os.path.abspath(os.path.dirname(__file__))))))
 from TH_Project.singletonTH.th_main import th_model
 
@@ -20,28 +20,35 @@ pymysql.version_info = (1, 3, 13, "final", 0)
 pymysql.install_as_MySQLdb()
 
 def home(request):
-    TH = th_model.instance()
-    run_state = TH.getRunState()
-    pi_date = TH.getPiDate() 
+    TH = th_model.instance() #th_model의 인스턴스 생성?
+    run_state = TH.getRunState() #th_model의 getRunState()메소드... run_state값 반환
+    pi_date = TH.getPiDate() # pi_date값 반환 (현재 시간?)
 
-    th_list = TH_data.objects.all().order_by('-id')
-    th_state = TH_state.objects.first()
+    th_list = TH_data.objects.all().order_by('-id') # th_data를 id 내림차순으로 정렬 (가장 최근 데이터부터 정렬)
+    th_state = TH_state.objects.first() #th_state의 처음에 있는 데이터 가지고 오기
+
+    #print(th_list)
 
     if run_state == 2:
-        th_update = TH_data.objects.last()
+        th_update = TH_data.objects.last() #가장 마지막에 측정된 데이터 불러옴
         if th_state and th_update:
-            th_state.end_time = th_update.run_time_date
-            th_state.save()
+            th_state.end_time = th_update.run_time_date #th_state의 end_time을 가장 마지막에 측정한 '진행시간'으로 변경
+            th_state.save() #값 수정후 저장
     
-    num = th_state.run_id
+    num = th_state.run_id #th_state의 run_id값
     th_list_mini = TH_data.objects.all().order_by('id')
-    num_max = TH_data.objects.last()
-    if num < 20 or num_max.run_id < 40:
-        th_list_mini = TH_data.objects.all().order_by('id')[:40]
-    elif TH_data.objects.filter(run_id=num+20).exists() == False and num_max.run_id > 40:
-        th_list_mini = TH_data.objects.all().order_by('id')[num_max.run_id-40:num_max.run_id]
+    num_max = TH_data.objects.last() #가장 마지막에 측정된 th_data 데이터
+    if num < 20 or num_max.run_id < 40: #th_state의 run_id값이 20미만 || 가장 최근 측정한 데이터가 40개 이하일때
+        th_list_mini = TH_data.objects.all().order_by('id')[:40] # 오래된 데이터순으로 정렬하여, 40개 까지만 th_list_mini에 할당
+    elif TH_data.objects.filter(run_id=num+20).exists() == False and num_max.run_id > 40: # 전체 데이터에서? th_state의 run_id + 20에 해당하는 데이터가 없고 || 측정중인 데이터의 개수가 40개가 넘어갈 경우
+        th_list_mini = TH_data.objects.all().order_by('id')[num_max.run_id-40:num_max.run_id] # 40개 이후의 데이터들을 th_list_mini에 할당
     else:
-        th_list_mini = TH_data.objects.all().order_by('id')[num-20:num+20]
+        th_list_mini = TH_data.objects.all().order_by('id')[num-20:num+20] # 이전 데이터랑 같이 할당???
+    # run_state: 현재 pi의 진행시간
+    # th_list: 가장 최근 데이터부터 정렬된 th_data
+    # pi_date: 라즈베리파이의 현재시간?
+    # th_state: th_state 데이터
+    # th_state_mini: th_data의 가장 최신 40개?의 데이터
     return render(request,'home.html',{'run_state':run_state, 'th_list':th_list,
                                        'pi_date':pi_date, 'th_state':th_state, 'th_list_mini':th_list_mini})
 
