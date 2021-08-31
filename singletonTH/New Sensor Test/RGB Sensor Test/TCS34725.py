e563a1ba13afbd516cc662b421870fc90b4cb271
JungSubeom <teller75@naver.com>
2021-08-31 10:55:13 +0900

    Sensor Test

---
+++ singletonTH/New Sensor Test/RGB Sensor Test/TCS34725.py
@@ -0,0 +1,21 @@
+import time
+import board
+import adafruit_tcs34725
+
+i2c = board.I2C()
+sensor = adafruit_tcs34725.TCS34725(i2c)
+
+while True:
+    color = sensor.color
+    color_rgb = sensor.color_rgb_bytes
+    print(
+        "RGB color as 8 bits per channel int: #{0:02X} or as 3-tuple: {1}".format(
+            color, color_rgb
+        )
+    )
+    
+    temp = sensor.color_temperature
+    lux = sensor.lux
+    print("Temperature: {0}K Lux: {1}\n".format(temp, lux))
+    # Delay for a second and repeat.
+    time.sleep(3.0)
